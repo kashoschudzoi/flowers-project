133da63ebdb94658e0853316d9f1f8ef45bca17d
kashoschudzoi <kashia.giorgi08@gmail.com>
2024-04-06 00:41:13 -0700

    clone

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Header from "@/components/header";
+import List from "@/components/list";
+import Image from "next/image";
+
+export default function Home() {
+  return (<>
+    <Header/>
+    <List/>
+</>
+  );
+}
